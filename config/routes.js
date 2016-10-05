fb10a2c7c38ff8c2acbd8cf55244fcd408039f3e
sutejoramadhan <ramadhansutejo94@gmail.com>
2016-10-06 00:34:39 +0700

    fix routing and add url friendly test routing

--- config/routes.js
+++ config/routes.js
@@ -3,5 +3,8 @@ var frontend = require('../app/controllers/frontend');
 module.exports = function(app) {
 
     app.get('/', frontend.index);
+    app.get('/index.pug', frontend.index);
+
+    app.get('/test-12-3', frontend.test);
 
 }
